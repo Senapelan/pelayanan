02c6a4cc105bbae499fbe7d61608af228b911d5b
Senapelan <arsip.senapelan@gmail.com>
2026-01-31 19:55:20 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,39 @@
 function openMenu(){
   document.getElementById("mobileMenu").classList.add("active");
+  document.querySelector(".menu-overlay").classList.add("active");
 }
 
 function closeMenu(){
   document.getElementById("mobileMenu").classList.remove("active");
+  document.querySelector(".menu-overlay").classList.remove("active");
 }
+
+/* klik luar = tutup menu */
+document.addEventListener("click", function(e){
+  const menu = document.getElementById("mobileMenu");
+  const btn  = document.querySelector(".menu-btn");
+
+  if(
+    menu.classList.contains("active") &&
+    !menu.contains(e.target) &&
+    !btn.contains(e.target)
+  ){
+    closeMenu();
+  }
+});
 
 function toggleSub(id){
   const sub = document.getElementById(id);
 
-  // tutup submenu lain (biar rapi)
   document.querySelectorAll(".submenu").forEach(el=>{
     if(el !== sub){
       el.style.display = "none";
     }
   });
 
-  // toggle submenu yang diklik
   sub.style.display = sub.style.display === "block" ? "none" : "block";
 }
 
+/* header scroll */
 window.addEventListener("scroll",()=>{
-  document.getElementById("header")
-    .classList.toggle("scrolled",window.scrollY>50);
-});
+  document.getE
